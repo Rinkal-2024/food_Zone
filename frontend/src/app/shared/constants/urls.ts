8db7c3e5f40f13523a05561e363435dfd1cfb1d1
Rinkal-2024 <rinkal.dhankariya@iflair.com>
2024-04-23 15:59:44 +0530

    update

--- frontend/src/app/shared/constants/urls.ts
+++ frontend/src/app/shared/constants/urls.ts
@@ -1,4 +1,4 @@
-const BASE_URL = "https://food-zone-server-nine.vercel.app"
+const BASE_URL = "http://localhost:7000"
 
 export const FOOD_URL = BASE_URL +'/api/food';
 export const FOOD_TAG_URL = BASE_URL +'/api/food/tag';
@@ -6,10 +6,8 @@ export const FOOD_BY_SEARCH_URL = BASE_URL +'/api/food/search/';
 export const FOOD_BY_TAG_URL = BASE_URL +'/api/food/tag/';
 export const FOOD_BY_ID_URL = BASE_URL +'/api/food/';
 
-
 export const USER_LOGIN_URL = BASE_URL + '/api/users/login';
 export const USER_REGISTER_URL = BASE_URL + '/api/users/register';
-
 
 export const ORDERS_URL = BASE_URL + '/api/orders';
 export const ORDER_CREATE_URL = ORDERS_URL + '/create';
@@ -17,5 +15,4 @@ export const ORDER_NEW_FOR_CURRENT_USER_URL = ORDERS_URL + '/newOrderForCurrentU
 export const ORDER_PAY_URL = ORDERS_URL + '/pay';
 export const ORDER_TRACK_URL = ORDERS_URL + '/track/';
 
-
-export const USER_UPLOAD_PROFILE_PICTURE_URL = BASE_URL + '/api/users/profilePicture'
+export const USER_UPLOAD_PROFILE_PICTURE_URL = BASE_URL + '/api/users/profilePicture';
